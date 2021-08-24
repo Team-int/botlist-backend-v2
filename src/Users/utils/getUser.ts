23284fa64c17d2cd77160a2677433787f756344b
onion8513 <jsi090224@naver.com>
2021-08-24 22:03:41 +0900

    fix(graphql): error handling

--- src/Users/utils/getUser.ts
+++ src/Users/utils/getUser.ts
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
-import { UserResult } from 'typings/getUser'
+import { GetUserReturnType, UserResult } from 'typings/getUser'
 
-export default async(accessToken: string): Promise<UserResult | null> => {
+export default async(accessToken: string): Promise<GetUserReturnType | null> => {
   try {
     const userResult = await fetch('https://discord.com/api/users/@me', {
       headers: {
@@ -9,6 +9,7 @@ export default async(accessToken: string): Promise<UserResult | null> => {
       }
     })
     const { id }: UserResult = await userResult.json()
+    if(!id) return null
     return { id }
   } catch(err) {
     return null
